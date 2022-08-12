4dd16b43404df529812665242faa8952f612b019
Cody P Schafer <dev@codyps.com>
2022-08-12 11:30:15 -0400

    warning killing and unsafe fn marking

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-use std::mem::MaybeUninit;
-
+#![deny(unsafe_op_in_unsafe_fn)]
 // struct A;
 // void a_create(A**);
 // void a_free(A*);
 // void a_print(A*)
-
 pub use a1::A;
 
 #[no_mangle]
@@ -18,6 +16,6 @@ pub extern "C" fn a_print(v: &A) {
 }
 
 #[no_mangle]
-pub extern "C" fn a_free(v: *mut A) {
+pub unsafe extern "C" fn a_free(v: *mut A) {
     unsafe { Box::from_raw(v) };
 }
